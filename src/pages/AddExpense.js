6a4abdd09c0cc2e99b4b092a654a69421a45eb71
Abe Malla <abe.malla8@gmail.com>
2018-04-28 22:30:46 -0700

    Added all components to AddExpense page

--- src/pages/AddExpense.js
+++ src/pages/AddExpense.js
@@ -4,12 +4,14 @@ import {
     SafeAreaView,
     TextInput,
     DatePickerIOS,
+    Text,
+    View,
 } from 'react-native';
 import AppStyle from '../styles/AppStyle';
 
 import Header from '../components/Header';
 import BigButton from '../components/BigButton';
-import CPStatusBar from '../components/CPStatusBar';
+import CPStatusBar, { MyStatusBar } from '../components/CPStatusBar';
 
 export default class AddExpense extends React.Component {
     constructor() {
@@ -30,25 +32,54 @@ export default class AddExpense extends React.Component {
         this.setState({ expenseDate: newDate });
     };
 
+    goBackToHome = () => {
+        this.props.navigation.goBack();
+    };
+
     render() {
         return (
+            // TODO: ADD STATUSBAR,
+            // WRAP BOTTOM PART IN NEW VIEW SO U CAN SET BACKGROUND COLOR
             <SafeAreaView style={styles.container}>
-                <Header text='Add Expense' backgroundColor={AppStyle.grey} />
-                <TextInput
-                    style={styles.reasonTextInput}
-                    placeholder='Add reason'
-                    value={this.state.reasonText}
-                    onChangeText={reasonText => this.setState({ reasonText })}
-                    maxLength={180}
-                    numberOfLines={3}
-                    returnKeyLabel='next'
-                />
-                
-                <DatePickerIOS
-                    mode="date"
-                    date={this.state.expenseDate}
-                    onDateChange={this.setDate}
-                    maximumDate={this.curDate}
+                <MyStatusBar backgroundColor={AppStyle.grey} />
+                <Header text="Add Expense" backgroundColor={AppStyle.grey} />
+
+                <View style={{backgroundColor:AppStyle.white, flex: 1}}>
+                    <TextInput
+                        style={styles.reasonTextInput}
+                        placeholder="Reason"
+                        value={this.state.reasonText}
+                        onChangeText={reasonText =>
+                            this.setState({ reasonText })
+                        }
+                        maxLength={120}
+                        multiline
+                        numberOfLines={3}
+                        returnKeyLabel="next"
+                    />
+
+                    <Text style={styles.textTitle}>Expense Cost</Text>
+                    <TextInput
+                        style={styles.priceTextInput}
+                        placeholder="$0.00"
+                        value={this.state.price ? this.state.price : undefined}
+                        onChangeText={price => this.setState({ price })}
+                        keyboardType="numeric"
+                    />
+
+                    <Text style={styles.textTitle}>Expense Date</Text>
+                    <DatePickerIOS
+                        mode="date"
+                        date={this.state.expenseDate}
+                        onDateChange={this.setDate}
+                        maximumDate={this.curDate}
+                    />
+                </View>
+
+                <BigButton
+                    onPress={this.goBackToHome}
+                    text="Add"
+                    backgroundColor={AppStyle.primaryColor}
                 />
             </SafeAreaView>
         );
@@ -57,10 +88,25 @@ export default class AddExpense extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-
+        backgroundColor: AppStyle.grey,
+        flex: 1,
     },
     reasonTextInput: {
         margin: 15,
+        fontSize: 18,
+        borderBottomWidth: 0.5,
+        paddingBottom: 15,
+        borderColor: AppStyle.greyLight,
+    },
+    textTitle: {
+        textAlign: 'center',
+        margin: 10,
+        color: AppStyle.black,
         fontSize: 20,
+    },
+    priceTextInput: {
+        alignSelf: 'center',
+        fontSize: 20,
+        margin: 10,
     },
 });
