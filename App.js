08f370af0e4c1e8e2d99bbc2550f8a22a051b18f
Abe Malla <abe.malla8@gmail.com>
2018-04-28 20:07:12 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+    render() {
+        return <View />;
+    }
+}
